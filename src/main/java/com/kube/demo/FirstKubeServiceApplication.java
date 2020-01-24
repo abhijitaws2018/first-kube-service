d21813df4ef2dbd846bcebe23ac26bb807977648
chakraa <chakraa@TCCCHAKRABORTYA.HibbertNet.com>
2020-01-23 23:53:06 -0500

    update

--- src/main/java/com/kube/demo/FirstKubeServiceApplication.java
+++ src/main/java/com/kube/demo/FirstKubeServiceApplication.java
@@ -1,7 +1,11 @@
 package com.kube.demo;
 
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
 import java.util.Date;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FirstKubeServiceApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(FirstKubeServiceApplication.class);
+	
 	@RequestMapping("/")
-	public String hello() {
-		return "Hello Kubernetes!! finally !!! Joyka, kemon acchis !!!!\n "
-				+ new Date() + "\n";
+	public String hello() throws UnknownHostException {
+		String message = "Hello Kubernetes!! finally !!! Joyka, kemon acchis !!!!\n "
+				+ new Date() + ". Server name : " + Inet4Address.getLocalHost().getHostName() +"\n";
+		LOGGER.info(message);
+		return message;
 	}
 	
 	public static void main(String[] args) {
